1b73a803a13e965d09ecdb3b5eceb0cabadd77e1
movavi_school <selleran@list.ru>
2022-10-23 12:49:09 +0700

    23 Октября

---
+++ lesson 6/lesson 6.py
@@ -0,0 +1,31 @@
+# x = ["Антон", "Данил", "Кирилл"]
+# x.append("Вова")
+#
+# if len(x) == 3:
+#     print(3)
+# elif len(x) == 4:
+#     print("Ты что-то сюда добавил?")
+# month = int(input("Введи номер месяца: "))
+# if month >=1 and month <= 12:
+#
+#     if month >= 3 and month <=5:
+#         print("Весна")
+#     if month >= 9 and month <=8:
+#         print("Лето")
+#     if month >= 9 and month <=11:
+#             print("Осень")
+#     else:
+#             print("Зима")
+# else:
+#     print("Месяца от 1 до 12,вообще-то")
+# h = int(input("Часы: "))
+# m = int(input("Минуты"))
+# s = int(input("Секунды"))
+# if 0 <= h <= 23 and 0 <= m <= 59 and 0 <= s <= 59:
+#     print("Формат верный")
+# else:
+#     print("Формат не верный")
+#     if h < 0 or h > 23:
+#         print("Часы некорректны [0, 23")
+#     if m < 0 or m > 59:
+#         print("Минуты некорректны [0, 59")
